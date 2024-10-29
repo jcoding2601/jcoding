acafcb955a40639b39ada1bf6bf95d16cfa888a9
제이코딩학원 <master@jcoding.co.kr>
2024-10-30 08:44:26 +0900

    Update readme.cmd

--- Database/readme.cmd
+++ Database/readme.cmd
@@ -1,1 +1,8 @@
 데이터베이스 관련 자료 입니다.
+
+1. Microsoft SQL Server Download
+    https://www.microsoft.com/ko-kr/sql-server/sql-server-downloads
+2. MySQL Download
+    https://dev.mysql.com/downloads/mysql/
+3. Mariadb Download
+    https://mariadb.org/download/?t=mariadb&p=mariadb&r=11.5.2&os=windows&cpu=x86_64&pkg=msi&mirror=blendbyte#entry-header
