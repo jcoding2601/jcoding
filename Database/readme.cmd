73906dbf7a7a181d4bfadfe76ceb3b7bc4232928
제이코딩학원 <master@jcoding.co.kr>
2024-10-30 09:45:25 +0900

    Update readme.cmd

--- Database/readme.cmd
+++ Database/readme.cmd
@@ -2,6 +2,12 @@
 
 1. Microsoft SQL Server Download
     https://www.microsoft.com/ko-kr/sql-server/sql-server-downloads
+
+    AdventureWorks 예제 데이터베이스
+    OLTP 데이터는 가장 일반적인 온라인 트랜잭션 워크로드용입니다.
+    https://learn.microsoft.com/ko-kr/sql/samples/adventureworks-install-configure?view=sql-server-ver16&tabs=ssms
+
+
 2. MySQL Download
     https://dev.mysql.com/downloads/mysql/
 3. Mariadb Download
